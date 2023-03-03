b11a55dd306da15e8b17eb9ce1d451a84e3beb1a
sarahpaulll <spaull@ucsd.edu>
2023-03-02 16:29:53 -0800

    h

--- grade.sh
+++ grade.sh
@@ -3,4 +3,31 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
-if [[-f ]]
+
+
+cd student-submission
+    if [[ -f ListExamples.java ]]
+    then
+    echo "List Examples found"
+    else
+    echo "need file ListExamples.java"
+    exit 1
+    fi 
+cd ..
+
+mkdir grader
+cp TestListExamples.java grader
+cp student-submission/ListExamples.java grader
+cp -r lib grader
+
+cd grader
+javac -cp ..:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+if [[ $? -eq 0 ]]
+then
+echo "compile succeeds"
+else
+echo "compilation error"
+exit 1
+fi
+
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
